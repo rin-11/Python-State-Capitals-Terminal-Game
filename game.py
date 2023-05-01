91e20adb485e665bab60fa95838b720d1e32bffe
Rin Kates <erinkates11@gmail.com>
2023-04-30 21:24:18 -0400

    state data entered

--- game.py
+++ game.py
@@ -3,3 +3,15 @@ print("Welcome to State Capitals Trivia!")
 print('Enter your name to get started: ')
 user_name = input()
 print(f'Welcome, {user_name}! Type the capital for each state randonly selected and press enter.')
+
+states = ['Alabama','Alaska','Arizona','Arkansas','California','Colorado','Connecticut','Delaware','Florida','Georgia','Hawaii','Idaho','Illinois','Indiana','Iowa','Kansas','Kentucky','Louisiana','Maine','Maryland','Massachusetts','Michigan','Minnesota','Mississippi','Missouri','Montana','Nebraska','Nevada','New Hampshire','New Jersey','New Mexico','New York','North Carolina','North Dakota','Ohio','Oklahoma','Oregon','Pennsylvania','Rhode Island','South Carolina','South Dakota','Tennessee','Texas','Utah','Vermont','Virginia','Washington','West Virginia','Wisconsin','Wyoming']
+
+capitals = ("Montgomery","Juneau","Phoenix","Little Rock","Sacramento","Denver","Hartford","Dover","Tallahassee","Atlanta","Honolulu","Boise","Springfield","Indianapolis","Des Moines","Topeka","Frankfort","Baton Rouge","Augusta","Annapolis","Boston","Lansing","St. Paul","Jackson","Jefferson City","Helena","Lincoln","Carson City","Concord","Trenton","Santa Fe","Albany","Raleigh","Bismarck","Columbus","Oklahoma City","Salem","Harrisburg","Providence","Columbia","Pierre","Nashville","Austin","Salt Lake City","Montpelier","Richmond","Olympia","Charleston","Madison","Cheyenne")
+
+state_data = []
+
+for i in range(len(states)):
+    state_data.append({
+        'state': states[i],
+        'capital': capitals[i]
+    })
