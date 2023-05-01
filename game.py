c211c227c56636db2d9e5ee926b9f652eb3792b6
Rin Kates <erinkates11@gmail.com>
2023-04-30 21:22:01 -0400

    set up complete

---
+++ game.py
@@ -0,0 +1,5 @@
+print("Welcome to State Capitals Trivia!")
+
+print('Enter your name to get started: ')
+user_name = input()
+print(f'Welcome, {user_name}! Type the capital for each state randonly selected and press enter.')
